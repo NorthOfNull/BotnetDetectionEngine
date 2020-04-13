84717baf401806b7dbe09dfa0f68bbf9846e6582
NorthOfNull <kyle.fryer71@gmail.com>
2020-04-14 00:49:14 +0100

    Argparse update; added new argument options (debug and read file)

--- botnet_detection_engine.py
+++ botnet_detection_engine.py
@@ -6,22 +6,6 @@ import argparse
 from detection_engine_modules.Detector import Detector
 
 
-
-#### TODO
-#### EXPORT THIS TO STDIN CONTROLLER MODULE
-######
-######  OR USE FOR STDIN INPUT FOR TESTING? 
-######
-###def get_stdin_netflow():
-###    netflow = ''
-###    
-###    while not netflow.endswith('\n'):
-###        netflow += sys.stdin.read(1)
-###
-###    return netflow[:-1]
-
-
-
 #### TODO
 #### EXPORT THIS TO IT'S OWN MODULE OR CLASS??????  -----> TO cmd_line_args.py
 '''
@@ -29,49 +13,16 @@ Parses the command line arguments
 @returns A populated namespace object, containing the parsed arguments from sys.argv
 '''
 def get_cmd_line_args():
-	parser = argparse.ArgumentParser(usage="./run.sh [-h --help] [--gui] [--log] ",
-									 description="The Botnet Detection Engine. GUI and logging is enabled by default.")
+	parser = argparse.ArgumentParser(description="The Botnet Detection Engine. GUI and logging is enabled by default.")
 
-	parser.add_argument("--gui", action="store_true", help="Disables GUI.")
-	parser.add_argument("--log", action="store_true", help="Disables alert and flow logging to file.")
+	parser.add_argument("-g", "--gui", action="store_false", help="Disables GUI.", default=True)
+	parser.add_argument("-l", "--log", action="store_false", help="Disables alert and flow logging to file.", default=True)
+	parser.add_argument("-d", "--debug", action="store_true", help="Enable verbose debugging output.", default=False)
+	parser.add_argument("-r", "--read", action="store", help="Read from \'.pcap\' or Network Flow file.", default=False)
 
 	args = parser.parse_args()
 
 	return args
-
-'''
-Updates the global variables depending upon the presence of the parsed
-command line arguments.
-'''
-def update_global_vars(args):
-	global Debug
-
-	if(args.gui == True):
-		# Disable GUI        
-		global GUI
-		GUI = False
-
-		if(Debug):
-			print("[ Cmd_Line_Args ] Disabling GUI!")
-
-	if(args.log == True):
-		# Disable alert and flow logging
-		global Logging
-		Logging = False
-
-		if(Debug):
-			print("[ Cmd_Line_Args ] Disabling Logging!")
-
-	return 0
-
-
-# Global Variable declarations
-# Set to default values (in the case of no command line args)
-GUI = True
-Logging = True
-
-# Debugging
-Debug = True
 
 
 # Program entry point
@@ -79,8 +30,10 @@ if __name__ == "__main__":
 	# Parse command line arguments
 	args = get_cmd_line_args()
 
+	print(args)
+
 	# Handle command line arguments
-	update_global_vars(args)
+	#update_global_vars(args)
 
 	# TODO
 	# TODO
@@ -89,5 +42,5 @@ if __name__ == "__main__":
 	# Model Object creation
 	# Model object de-serialisation of saved ML model files
 	# Detector object init (which loads models)
-	detector = Detector(GUI, Logging, Debug)
+	detector = Detector(args)
 	detector.run()
