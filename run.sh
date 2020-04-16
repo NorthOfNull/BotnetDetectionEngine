190f14a8fabd4f11695895ac1b068fa8c1f1dc0f
NorthOfNull <kyle.fryer71@gmail.com>
2020-04-16 01:03:13 +0100

    Run script refactoring

--- run.sh
+++ run.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-function show_help {
-	sudo python3 botnet_detection_engine.py --help
-}
-
 # Check number of cmd line args and limit
 if [ $# -gt 5 ]
 then
 	show_help
 	echo; echo; echo "Exceeded maximum number of command line arguments (5)"; echo;
 else
-	# Check if '--help' is requested
-	if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$2" = "-h" ] || [ "$2" = "--help" ]
-	then
-		show_help
-	else
-		# Starts electron nodejs instance via main.js
-		# Do not run GUI if -n switch is present
-		if [ "$1" != "--no-gui" ] && [ "$2" != "--no-gui" ]
+	# Starts electron nodejs instance via main.js
+	# Do not run GUI if -n or --no-gui switch is present
+	gui=True
+
+	for arg in "$@"
+	do
+		if [ "$arg" = "--no-gui" ] || [ "$arg" = "-g" ]
 		then
-			npm start &
+			gui=False
+			break
 		fi
+	done
 
-
-		# Executes the detection engine
-		# Default (no command line arguments) sends labelled flows to electron 
-		# instance for data data processing for the user, without logging to a file
-		sudo python3 botnet_detection_engine.py $@
+	if [ "$gui" = True ]
+	then
+		npm start &
 	fi
+
+	# Executes the detection engine
+	# Default (no command line arguments) sends labelled flows to electron 
+	# instance for data data processing for the user
+	sudo python3 botnet_detection_engine.py $@
 fi
-
-
-
