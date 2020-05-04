846ebab168e387ca4f2c0c7c8f2a40f36447c40a
NorthOfNull <kyle.fryer71@gmail.com>
2020-05-04 22:10:57 +0100

    Unit testing updates

--- unit_test.py
+++ unit_test.py
@@ -2,9 +2,12 @@ import unittest
 
 # Test files
 from testing.LoggerTest import bb_LoggerTest, wb_LoggerTest
+<<<<<<< HEAD
 from testing.SnifferTest import bb_SnifferTest, wb_SnifferTest
 from testing.Websocket_Client_Test import bb_ClientTest, wb_ClientTest 
 
+=======
+>>>>>>> 42003dbced9e69a67678c4367273e4c62288a474
 
 if __name__ == '__main__':
 	unittest.main()
